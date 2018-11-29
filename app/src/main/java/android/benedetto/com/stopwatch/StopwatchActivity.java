a56a22b47f2dd1ef58108d4f5744e94838c1d67e
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-11-30 00:00:19 +0100

    Initial commit

---
+++ app/src/main/java/android/benedetto/com/stopwatch/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package android.benedetto.com.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
